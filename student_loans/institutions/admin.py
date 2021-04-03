a7acd6d70e56332529fc0685e60e49090dce74fb
Andrew Mitchell <andrewpmitchell7@gmail.com>
2021-04-02 20:54:58 -0400

    Initial commit - Models for institutions

---
+++ student_loans/institutions/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Institution, Award
+
+admin.site.register(Institution)
+admin.site.register(Award)
---
+++ student_loans/institutions/models.py
@@ -0,0 +1,46 @@
+import uuid
+
+from django.db import models
+
+
+class Institution(models.Model):
+
+    class Type(models.TextChoices):
+        PUBLIC = 'Public'
+        PRIVATE_NONPROFIT = 'Private Nonprofit'
+        PROPRIETARY = 'Proprietary'
+        FOREIGN_FOR_PROFIT = 'Foreign For Profit'
+        FOREIGN_PRIVATE = 'Foreign Private'
+        FOREIGN_PUBLIC = 'Foreign Public'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    state = models.CharField(max_length=2) # TODO: Can this be an enum or choice
+    zip = models.CharField(max_length=9)
+    type = models.CharField(max_length=255, choices=Type.choices)
+    source_id = models.CharField(max_length=8)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Award(models.Model):
+
+    class LoanType(models.TextChoices):
+        DIRECT_SUBSIDIZED = 'Direct Loan Subsidized'
+        DIRECT_UNSUBSIDIZED_UNDERGRADUATE = 'Direct Loan Unsubsidized - Undergraduate'
+        DIRECT_UNSUBSIDIZED_GRADUATE = 'Direct Loan Unsubsidized - Graduate'
+        DIRECT_PARENT_PLUS = 'Direct Loan Parent Plus'
+        DIRECT_GRAD_PLUS = 'Direct Loan Grad Plus'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    quarter = models.CharField(max_length=6)
+    loan_type = models.CharField(max_length=50, choices=LoanType.choices)
+    receipient_count = models.IntegerField()
+    loans_originated_count = models.IntegerField()
+    loans_originated_amount = models.DecimalField(max_digits=11, decimal_places=2)
+    disbursements_count = models.IntegerField()
+    disbursements_amount = models.DecimalField(max_digits=11, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
